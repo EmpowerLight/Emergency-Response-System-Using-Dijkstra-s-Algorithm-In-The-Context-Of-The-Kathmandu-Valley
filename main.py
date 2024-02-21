c41e4a4d2e8161ab12978988c8fe84f6286dd20c
EmpowerLight <ankit.shrestha196@gmail.com>
2024-02-21 18:06:27 +0545

    Add database

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+import secrets
 import pandas as pd
 import numpy as np
 from shapely.geometry import Point
@@ -7,9 +10,39 @@ import concurrent.futures
 import ast
 from reportlab.pdfgen import canvas
 from io import BytesIO
+from flask import Flask, render_template, request, jsonify, send_file, request, flash, redirect, url_for
+
 
 app = Flask(__name__)
+secret_key = secrets.token_hex(32)
+app.config['SECRET_KEY'] = secret_key 
 
+
+##CONNECT TO DB
+db_name = "user.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+db = SQLAlchemy(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fullname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    phone_number = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371.0  # Earth radius in kilometers
@@ -162,15 +195,60 @@ def generate_pdf():
     return send_file(pdf_buffer, as_attachment=True, download_name='patient_report.pdf')
 
 
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["pwd"]
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for("home"))
     return render_template("login.html")
 
 
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        if User.query.filter_by(email=request.form["email"]).first():
+            # User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+        first_name = request.form["fname"]
+        last_name = request.form["lname"]
+        full_name = f"{first_name} {last_name}"
+        email = request.form["email"]
+        phone_number = request.form["phone"]
+        password = request.form["pwd"]
+        hashed_password = generate_password_hash(
+            password, 
+            method='pbkdf2:sha256', 
+            salt_length=8
+            )
+        new_user = User(
+            fullname=full_name,
+            email=email,
+            phone_number = phone_number,
+            password=hashed_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for("home"))
+    return render_template("register.html", current_user=current_user)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route("/map")
@@ -200,7 +278,7 @@ def contactus():
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", current_user=current_user)
 
 if __name__ == "__main__":
     app.run(debug=True)
